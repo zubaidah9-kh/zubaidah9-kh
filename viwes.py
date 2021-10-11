4a24a8725b32cdb75b462af7f1c6b4971b132fc9
zubaidah9-kh <92298216+zubaidah9-kh@users.noreply.github.com>
2021-10-11 17:40:15 +0300

    Update viwes.py

--- viwes.py
+++ viwes.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def week3_assignment(request):
-    return render(request, 'week3/base.html', {})
+    return render(request, 'base.html', {})
